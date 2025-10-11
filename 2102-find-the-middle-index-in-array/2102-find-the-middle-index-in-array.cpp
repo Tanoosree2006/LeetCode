9230b3413bc1aec5e9998e3de950e543b1ec4d16
Tanoosree2006 <147473260+Tanoosree2006@users.noreply.github.com>
2025-10-11 21:56:53 +0530

    Time: 0 ms (100%), Space: 16 MB (58.98%) - LeetHub

--- 2102-find-the-middle-index-in-array/2102-find-the-middle-index-in-array.cpp
+++ 2102-find-the-middle-index-in-array/2102-find-the-middle-index-in-array.cpp
@@ -6,8 +6,8 @@ public:
             int sum=0;
             int sums=0;
             for(int j=0;j<i;j++) sum+=nums[j];
-            for(int j=i+1;j<n;j++)sums+=nums[j];
-                if(sum==sums) return i;
+            for(int j=i+1;j<n;j++) sums+=nums[j];
+            if(sum==sums) return i;
         }
         return -1;
     }
