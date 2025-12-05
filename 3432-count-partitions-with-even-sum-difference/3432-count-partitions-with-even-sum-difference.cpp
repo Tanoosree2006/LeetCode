feff1009f284244aacebde205788c33293bd81f5
Tanoosree2006 <147473260+Tanoosree2006@users.noreply.github.com>
2025-12-05 11:15:59 +0530

    Time: 0 ms (100%), Space: 22.4 MB (53.79%) - LeetHub

--- 3432-count-partitions-with-even-sum-difference/3432-count-partitions-with-even-sum-difference.cpp
+++ 3432-count-partitions-with-even-sum-difference/3432-count-partitions-with-even-sum-difference.cpp
@@ -5,10 +5,10 @@ public:
         int cnt = 0;
         long left = 0, right = 0;
 
-        // step 1: initialize right with total sum
-        for (int x : nums) right += x;
+        for(int i=0;i<n;i++){
+            right+=nums[i];
+        }
 
-        // step 2: loop until second last element
         for (int i = 0; i < n - 1; i++) {
             left += nums[i];
             right -= nums[i];
