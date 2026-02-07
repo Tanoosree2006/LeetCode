0a27209093c1ed25c9acd04fe8f5cc07b5c7f514
Tanoosree2006 <147473260+Tanoosree2006@users.noreply.github.com>
2026-02-07 21:37:16 +0530

    Time: 55 ms (17.6%), Space: 55.1 MB (16.57%) - LeetHub

--- 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.cpp
+++ 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.cpp
@@ -7,17 +7,17 @@ public:
         int b_count=0;
         for(int i=0;i<n;i++){
             count_b[i]=b_count;
-            if(s[i]=='b') b_count++;
+            if(s[i]=='b')b_count++;
         }
-        int a_count =0;
+        int a_count=0;
         for(int i=n-1;i>=0;i--){
             count_a[i]=a_count;
             if(s[i]=='a') a_count++;
         }
-        int min_deletions=n;
+        int min_del=n;
         for(int i=0;i<n;i++){
-            min_deletions=min(min_deletions,count_a[i]+count_b[i]);
+            min_del=min(min_del,count_a[i]+count_b[i]);
         }
-        return min_deletions;
+        return min_del;
     }
 };
