ba65da57f8d96025d98771db678e0c8800404db8
Tanoosree2006 <147473260+Tanoosree2006@users.noreply.github.com>
2026-03-24 22:54:49 +0530

    Time: 62 ms (19.23%), Space: 34.5 MB (84.16%) - LeetHub

---
+++ 0599-minimum-index-sum-of-two-lists/0599-minimum-index-sum-of-two-lists.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<string> findRestaurant(vector<string>& list1, vector<string>& list2) {
+        vector<string> result;
+        int n=list1.size();
+        int m=list2.size();
+        int minsum=INT_MAX;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(list1[i]==list2[j]){
+                    if(i+j<minsum){
+                    result.clear();
+                    result.push_back(list1[i]);
+                    minsum=i+j;
+                }
+                else if(i+j==minsum){
+                    result.push_back(list1[i]);
+                }
+                }
+            }
+        }
+        return result;
+    }
+};
