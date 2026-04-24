055a60b59d97bd612d52981b7e7225683367093e
Tanoosree2006 <147473260+Tanoosree2006@users.noreply.github.com>
2026-04-24 19:15:10 +0530

    Time: 0 ms (100%), Space: 8.9 MB (90.95%) - LeetHub

--- 2833-furthest-point-from-origin/2833-furthest-point-from-origin.cpp
+++ 2833-furthest-point-from-origin/2833-furthest-point-from-origin.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     int furthestDistanceFromOrigin(string moves) {
-        int l=0,r=0,blank=0;
-        for(char c:moves){
-            if(c=='L') l++;
-            else if(c=='R') r++;
-            else blank++;
-        }
-        return abs(r-l)+blank;
+     int l=0,r=0,blank=0;
+     for(char c:moves){
+        if(c=='L') l++;
+        else if(c=='R') r++;
+        else blank++;
+     }   
+     return abs(r-l)+blank;
     }
 };
