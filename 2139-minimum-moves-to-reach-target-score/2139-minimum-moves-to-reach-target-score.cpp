a742806bad29a2a82399eb603683a27e936eadc7
Tanoo sree Krosuru <147473260+Tanoosree2006@users.noreply.github.com>
2026-07-01 21:59:20 +0530

    Time: 0 ms (100%), Space: 8 MB (37.11%) - LeetHub

--- 2139-minimum-moves-to-reach-target-score/2139-minimum-moves-to-reach-target-score.cpp
+++ 2139-minimum-moves-to-reach-target-score/2139-minimum-moves-to-reach-target-score.cpp
@@ -4,18 +4,15 @@ public:
         int count=0;
         while(target>1){
             if(maxDoubles==0){
-                count =count+(target-1);
+                count=count+(target-1);
                 break;
             }
-            if(target%2==0 && maxDoubles>0){
-                target=target/2;
+            if(target%2==0){
+                target/=2;
                 maxDoubles--;
-                count++;
             }
-            else {
-                target=target-1;
-                count++;
-            }
+            else target--;
+            count++;
         }
         return count;
     }
