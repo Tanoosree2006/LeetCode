b3b12159d2213d32280d0bd5f08ddbda76712d5a
Tanoo sree Krosuru <147473260+Tanoosree2006@users.noreply.github.com>
2026-07-04 10:36:40 +0530

    Time: 0 ms (100%), Space: 13.4 MB (64.2%) - LeetHub

--- 2293-min-max-game/2293-min-max-game.cpp
+++ 2293-min-max-game/2293-min-max-game.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     int minMaxGame(vector<int>& nums) {
-        int n= nums.size();
+        int n=nums.size();
         if(n==1) return nums[0];
-        vector<int> newNum(n/2);
+        vector<int>newNums(n/2);
         for(int i=0;i<n/2;i++){
+            if(i%2==0) newNums[i]=min(nums[2*i],nums[2*i+1]);
+            else newNums[i]=max(nums[2*i],nums[2*i+1]);
 
-            if(i%2==0) newNum[i]=min(nums[2*i],nums[2*i+1]);
-            else newNum[i]=max(nums[2*i],nums[2*i+1]);
         }
-        int res=minMaxGame(newNum);
+        int res=minMaxGame(newNums);
         return res;
     }
 };
