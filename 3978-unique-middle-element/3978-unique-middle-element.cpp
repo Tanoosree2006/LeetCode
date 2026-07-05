77a91465adac557f5b138867ab2e0f364b79effc
Tanoo sree Krosuru <147473260+Tanoosree2006@users.noreply.github.com>
2026-07-05 10:33:36 +0530

    Time: 0 ms (100%), Space: 31 MB (71.43%) - LeetHub

---
+++ 3978-unique-middle-element/3978-unique-middle-element.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool isMiddleElementUnique(vector<int>& nums) {
+        int n=nums.size();
+        int cnt=0;
+        int middle=nums[n/2];
+        for(int i=0;i<n;i++){
+            if(nums[i]==middle) cnt++;
+        }
+        return cnt==1;
+    }
+};
