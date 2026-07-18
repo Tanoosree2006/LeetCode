e2008e835757c01c0d68269d41ba501ad2cd5fb0
Tanoo sree Krosuru <147473260+Tanoosree2006@users.noreply.github.com>
2026-07-18 20:57:38 +0530

    Time: 8 ms (11.96%), Space: 23.8 MB (9.81%) - LeetHub

---
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+
+        stringstream ss(s);
+        string word, ans;
+
+        while (ss >> word) {
+            if (ans.empty())
+                ans = word;
+            else
+                ans = word + " " + ans;
+        }
+
+        return ans;
+    }
+};
