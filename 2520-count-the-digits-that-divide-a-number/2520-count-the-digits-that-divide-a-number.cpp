3504aae53373dd719b81c6c952b9ddf5dc8bf7f9
Tanoo sree Krosuru <147473260+Tanoosree2006@users.noreply.github.com>
2026-09-18 12:27:00 +0530

    Time: 0 ms (100%), Space: 7.9 MB (5.26%) - LeetHub

---
+++ 2520-count-the-digits-that-divide-a-number/2520-count-the-digits-that-divide-a-number.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int countDigits(int num) {
+        int n = num;
+        int cnt = 0;
+        while(n > 0) {
+            int digit = n % 10;
+            if(num % digit == 0)
+                cnt++;
+            n = n / 10;
+        }
+        return cnt;
+    }
+};
