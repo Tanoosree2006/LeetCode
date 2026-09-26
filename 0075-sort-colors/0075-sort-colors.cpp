248be3c0b0158052375b8d017c1e213b9a24468c
Tanoo sree Krosuru <147473260+Tanoosree2006@users.noreply.github.com>
2026-09-26 12:42:46 +0530

    Time: 0 ms (100%), Space: 11.7 MB (11.21%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -2,12 +2,10 @@ class Solution {
 public:
     void sortColors(vector<int>& nums) {
         int n=nums.size();
-        for(int i=0;i<n;i++){
-            for(int j=i+1;j<n;j++){
-                if(nums[i]>nums[j]){
-                    swap(nums[i],nums[j]);
+            for(int i=0;i<n;i++){
+                for(int j=i+1;j<n;j++){
+                    if(nums[i]>nums[j]) swap(nums[i],nums[j]);
                 }
             }
-        }
     }
 };
